26a29eb46d89062feed4a513b287f84b773ed30c
Gail Hernandez <arelaxingsaturdayafternoon@gmail.com>
2019-11-27 17:36:06 -0800

    Set up data model

--- vendordb/admin.py
+++ vendordb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from.models import Type, Region, City, Vendor, Offering
+
+admin.site.register(Type)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(Vendor)
+admin.site.register(Offering)
--- vendordb/models.py
+++ vendordb/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    """Category or type of service."""
+    text = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class Region(models.Model):
+    """General location (e.g., state, country, etc.)."""
+    text = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class City(models.Model):
+    """City."""
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'cities'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class Vendor(models.Model):
+    """An event vendor or supplier."""
+    text = models.CharField(max_length=200)
+    url = models.URLField
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class Offering(models.Model):
+    """Services or spaces offered by a vendor or supplier."""
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
